36ef6d1443b1cb3ac0c991230938c56973fb714b
arif-adnan <arif751803@gmail.com>
2023-08-15 15:29:18 +0600

    updated

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
-const http = require(`http`);
 const express = require("express");
 const app = express();
-const server = http.createServer(app);
-const socketio = require("socket.io");
-const io = socketio(server);
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
-const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const port = process.env.PORT || 5000;
-const socketPort = process.env.SOCKETPORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
 const DBUser = process.env.DBUser;
 const DBPassword = process.env.DBPassword;
-const uri = `mongodb+srv://${DBUser}:${DBPassword}@cluster0.qmcox.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${DBUser}:${DBPassword}@cluster0.jqo1zug.mongodb.net`
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -28,30 +22,29 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const database = client.db("vclass");
+    const database = client.db("vworkspace");
     const usersCollection = database.collection("users");
     const classesCollection = database.collection("classes");
     const projectsCollection = database.collection("projects");
+    const assignmentsCollection = database.collection("assignments");
+
 
     // POST USERS
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
+      console.log(user);
       res.json(result);
-      console.log(result);
     });
-
     app.get("/users", async (req, res) => {
       const cursor = usersCollection.find();
       const users = await cursor.toArray();
       res.json(users);
     });
-
     // UPDATE USERS
     app.put("/users/:email", async (req, res) => {
       const email = req.params.email;
       const updatedProfile = req.body;
-      console.log(updatedProfile);
       const filter = { email: email };
       const updateDoc = {
         $set: { type: updatedProfile.type },
@@ -62,33 +55,52 @@ async function run() {
         updateDoc,
         options
       );
+      console.log(updatedProfile);
       res.json(result);
-      console.log(result, "check if it works");
     });
-
     // GET USER BY EMAIL
     app.get("/users/:email", async (req, res) => {
       const email = req.params.email;
       const filter = { email: email };
       const user = await usersCollection.findOne(filter);
+      console.log(user, "ind");
       res.json(user);
     });
 
+
+    // ASSIGNMENT POST
+    app.post("/assignments", async (req, res) => {
+      const user = req.body;
+      const result = await assignmentsCollection.insertOne(user);
+      res.json(result);
+    });
+    // GET ASSIGNEMNTS
+    app.get("/assignments", async (req, res) => {
+      const cursor = assignmentsCollection.find();
+      const assignments = await cursor.toArray();
+      res.json(assignments);
+    });
+    // GET INDIVIDUAL ASSIGNEMNTS
+    app.get("/assignments/:postID", async (req, res) => {
+      const postID = req.params.postID;
+      const filter = { postID: postID };
+      const user = await assignmentsCollection.findOne(filter);
+      res.json(user);
+    });
+    
     // POST CLASSROOM
     app.post("/classes", async (req, res) => {
       const user = req.body;
       const result = await classesCollection.insertOne(user);
       res.json(result);
-      // console.log(result);
+      //  (result);
     });
-
-    // POST AN ANNOUNCEMENT IN CLASSROOM
-    app.put("/classes/posts/:code", async (req, res) => {
-      const code = req.params.code;
-      const announcementData = req.body;
-      const filter = { code: code };
+    // UPDATE STUDENT INFOR WHEN JOIN IN CLASSROOM
+    app.put("/classes/join/student", async (req, res) => {
+      const joinData = req.body;
+      const filter = { code: joinData.code };
       const updateDoc = {
-        $push: { posts: announcementData },
+        $push: { studentInfo: joinData },
       };
       // const options = { upsert: true };
       const result = await classesCollection.updateMany(
@@ -98,14 +110,24 @@ async function run() {
       );
       res.json(result);
     });
-
+    // UPDATE AN ANNOUNCEMENT IN CLASSROOM
+    app.put("/classes/posts/:code", async (req, res) => {
+      const code = req.params.code;
+      const announcementData = req.body;
+      const filter = { code: code };
+      const updateDoc = {
+        $push: { posts: announcementData },
+      };
+      // const options = { upsert: true };
+      const result = await classesCollection.updateMany(filter, updateDoc);
+      res.json(result);
+    });
     // GET CLASSROOM
     app.get("/classes", async (req, res) => {
       const cursor = classesCollection.find();
       const classes = await cursor.toArray();
       res.json(classes);
     });
-
     // GET CLASS WITH CODE
     app.get("/classes/:code", async (req, res) => {
       const code = req.params.code;
@@ -113,15 +135,6 @@ async function run() {
       const cl = await classesCollection.findOne(filter);
       res.json(cl);
     });
-
-    // GET INDIVIDUAL CLASS WITH ID
-    // app.get("/classroom/:postID", async (req, res) => {
-    //   const id = req.params.postID;
-    //   const filter = { postID: id };
-    //   const cl = await classesCollection.findOne(filter);
-    //   res.json(cl);
-    // });
-
     // INSERT CLASS INFO IN THE USERS
     app.put("/users/join/:email", async (req, res) => {
       const email = req.params.email;
@@ -134,9 +147,7 @@ async function run() {
       };
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.json(result);
-      console.log(result);
     });
-
     // ASSIGN PROJECT IN THE CLASS
     app.put("/classes/assign", async (req, res) => {
       const assign = req.body;
@@ -145,24 +156,19 @@ async function run() {
         $push: { assign },
       };
       const result = await classesCollection.updateOne(filter, updateDoc);
-      console.log(result);
     });
-
     // POST PROJECTS
     app.post("/projects", async (req, res) => {
       const user = req.body;
       const result = await projectsCollection.insertOne(user);
       res.json(result);
-      // console.log(result);
     });
-
     // GET PROJECTS
     app.get("/projects", async (req, res) => {
       const cursor = projectsCollection.find();
       const projects = await cursor.toArray();
       res.json(projects);
     });
-
     // GET PROJECT BY ID
     app.get("/projects/:id", async (req, res) => {
       const id = req.params.id;
@@ -170,8 +176,7 @@ async function run() {
       const result = await projectsCollection.findOne(query);
       res.json(result);
     });
-
-    // INSERT POSTS FRON PROJECTS CLASS
+    // INSERT POSTS FROM PROJECTS CLASS
     app.put("/projects/:id", async (req, res) => {
       const id = req.params.id;
       const post = req.body;
@@ -181,69 +186,18 @@ async function run() {
       };
       const result = await projectsCollection.updateOne(filter, updateDoc);
       res.json(result);
-      console.log(result);
     });
   } finally {
     // await client.close();
   }
 }
 
-io.on("connect", (socket) => {
-  socket.on("join", ({ name, room }, callback) => {
-    const { error, user } = addUser({ id: socket.id, name, room });
-
-    if (error) return callback(error);
-
-    socket.join(user.room);
-
-    socket.emit("message", {
-      user: "admin",
-      text: `${user.name}, welcome to room ${user.room}.`,
-    });
-    socket.broadcast
-      .to(user.room)
-      .emit("message", { user: "admin", text: `${user.name} has joined!` });
-
-    io.to(user.room).emit("roomData", {
-      room: user.room,
-      users: getUsersInRoom(user.room),
-    });
-
-    callback();
-  });
-
-  socket.on("sendMessage", (message, callback) => {
-    const user = getUser(socket.id);
-
-    io.to(user.room).emit("message", { user: user.name, text: message });
-
-    callback();
-  });
-
-  socket.on("disconnect", () => {
-    const user = removeUser(socket.id);
-
-    if (user) {
-      io.to(user.room).emit("message", {
-        user: "Admin",
-        text: `${user.name} has left.`,
-      });
-      io.to(user.room).emit("roomData", {
-        room: user.room,
-        users: getUsersInRoom(user.room),
-      });
-    }
-  });
-});
-
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello Virtual Class");
+  res.send( `Responsing from port ${port}!`);
 });
 
 app.listen(port, () => {
   console.log(`listening at ${port}`);
 });
-
-server.listen(socketPort, () => console.log(`Listening on port ${socketPort}`));
